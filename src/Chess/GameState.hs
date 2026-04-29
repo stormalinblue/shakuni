929b78b7aa6522273d039bf6bc41d20467c85a37
Muktaka Joshipura <muktaka2000@gmail.com>
2026-04-29 11:15:47 -0500

    feat(Chess): Begin filling out makeMove

--- src/Chess/GameState.hs
+++ src/Chess/GameState.hs
@@ -1,38 +1,52 @@
-module Chess.GameState (
-    BoardData,
+module Chess.GameState
+  ( GameState,
     Color,
-    PieceType) where
-
-import Chess.Position
-import Chess.Piece
+    PieceType,
+  )
+where
 
 import qualified Chess.GameState.Display as GSD
+import Chess.Piece
+import Chess.Position
 
+data BaseMove = BaseMove {from :: Position, to :: Position}
 
-data BaseMove = BaseMove { from :: Position, to :: Position }
 newtype Move = Move [BaseMove]
 
-data PlayerState = PlayerState { player :: Color, pieces :: [PieceState] }
+data PlayerState = PlayerState {player :: Color, pieces :: [PieceState]}
 
-
-data BoardData = BoardData {
-    whitePlayer :: PlayerState,
+data GameState = GameState
+  { whitePlayer :: PlayerState,
     blackPlayer :: PlayerState,
-    turn :: Color
-    -- halfMoveClock :: Int,
-    -- fullMoveNumber :: Int
-}
+    turn :: Color,
+    halfMoveClock :: Int,
+    fullMoveNumber :: Int
+  }
 
-class Board b where
-    isGameOver :: b -> Bool
-    -- turn :: b -> Color
-    castlingRights :: b -> [PieceState]
-    fullMoveNumber :: b -> Int
-    halfMoveClock :: b -> Int
-    legalMoves :: b -> [Move]
-    isCheck :: b -> Bool
-    givesCheck :: b -> Move -> Bool
-    givesCheckmate :: b -> Move -> Bool
-    isLegal :: b -> Move -> Bool
-    isCheckmate :: b -> Bool
-    canClaimDraw :: b -> Bool
+makeMove :: GameState -> Move -> GameState
+makeMove gs m =
+  gs
+    { turn = nextPlayer (turn gs),
+      fullMoveNumber = nextFMN (turn gs) (fullMoveNumber gs)
+    }
+  where
+    nextPlayer White = Black
+    nextPlayer Black = White
+
+    nextFMN :: Color -> Int -> Int
+    nextFMN White x = x
+    nextFMN Black x = 1 + x
+
+-- class Board b where
+--     isGameOver :: b -> Bool
+--     -- turn :: b -> Color
+--     castlingRights :: b -> [PieceState]
+--     fullMoveNumber :: b -> Int
+--     halfMoveClock :: b -> Int
+--     legalMoves :: b -> [Move]
+--     isCheck :: b -> Bool
+--     givesCheck :: b -> Move -> Bool
+--     givesCheckmate :: b -> Move -> Bool
+--     isLegal :: b -> Move -> Bool
+--     isCheckmate :: b -> Bool
+--     canClaimDraw :: b -> Bool
